b3be1da690da1113314751a2bbfb03008354bc9b
PabloLechuga <pablolechu98@gmail.com>
2018-12-18 12:54:23 +0100

    Primer commit del ejemplo

---
+++ ej1.java
@@ -0,0 +1,21 @@
+public class ej1 
+{
+	public static void main(String[] args)
+	{
+		int edad=2;
+		int nivel_de_estudios=4;
+		int ingresos=29000;
+		boolean jasp;
+		if (edad <= 28 && nivel_de_estudios > 3 && ingresos >28000)
+		{
+			jasp=true;
+			System.out.println(jasp);
+		}
+		else
+		{
+			jasp=false;
+			System.out.println(jasp);
+		}
+	}
+}
+
